3a23fc4c875728402b5245f7690f8aea5b2fa4e8
Yannis Maragos <ioannismaragos@gmail.com>
2021-06-08 21:41:40 +0300

    Added missing modal window for images

--- site/tmpl/slider/default.php
+++ site/tmpl/slider/default.php
@@ -1,82 +1,104 @@
 <?php
-/**
-* @title		Minitek Slider
-* @copyright	Copyright (C) 2011-2021 Minitek, All rights reserved.
-* @license		GNU General Public License version 3 or later.
-* @author url	https://www.minitek.gr/
-* @developers	Minitek.gr
-*/
 
-// no direct access
+/**
+ * @title		Minitek Slider
+ * @copyright	Copyright (C) 2011-2021 Minitek, All rights reserved.
+ * @license		GNU General Public License version 3 or later.
+ * @author url	https://www.minitek.gr/
+ * @developers	Minitek.gr
+ */
+
 defined('_JEXEC') or die('Restricted access');
 
+use Joomla\CMS\HTML\HTMLHelper;
+
 // Page title
-if ($this->slider_page_title)
-{
-	if ($this->params->get('show_page_heading', 1))
-	{
+if ($this->slider_page_title) {
+	if ($this->params->get('show_page_heading', 1)) {
 		$app = JFactory::getApplication();
 		$menu = $app->getMenu();
 		$active = $menu->getActive();
 
-		if ($active->params->get('page_heading'))
-		{
+		if ($active->params->get('page_heading')) {
 			$page_heading = $active->params->get('page_heading');
-		} 
-		else 
-		{
+		} else {
 			$doc = JFactory::getDocument();
 			$page_heading = $doc->getTitle();
 		}
-		
-		?><div class="page-header">
+
+?><div class="page-header">
 			<h1><?php echo $this->escape($page_heading); ?></h1>
-		</div><?php
+		</div>
+<?php
 	}
 }
 
 // Suffix
 $suffix = '';
 
-if (isset($this->suffix))
-{
+if (isset($this->suffix)) {
 	$suffix = $this->suffix;
 }
 
 // Wrapper padding
-if (!$this->cont_padding)
-{
+if (!$this->cont_padding) {
 	$padding = '0 0 1px 0';
-}
-else 
-{
-	$padding = $this->cont_padding.'px';
+} else {
+	$padding = $this->cont_padding . 'px';
 }
 
 // Slider Container
-?><div id="mslider_wrapper_<?php echo $this->widgetID; 
-	?>" class="mslider-wrapper" style="padding: <?php echo $padding; ?>;"><?php
+?><div id="mslider_wrapper_<?php echo $this->widgetID;
+							?>" class="mslider-wrapper" style="padding: <?php echo $padding; ?>;">
+	<?php
 
-	?><div class="mslider-wrapper-inner <?php
-	echo $this->horizontal_padding ? 'mslider_horizontal_padding' : ''; ?>"><?php 
-		
+	?><div class="mslider-wrapper-inner 
+	<?php
+	echo $this->horizontal_padding ? 'mslider_horizontal_padding' : ''; ?>">
+		<?php
+
 		// Main slider
-		?><div id="mslider_<?php echo $this->widgetID; 
-			?>" class="mslider mslider_main mslider_<?php echo $this->theme; ?> <?php 
-			echo ($this->bullets) ? 'with-bullets' : ''; ?> <?php
-			echo ($this->progressbar) ? 'with-progressbar' : ''; ?> <?php
-			echo $this->bullets_style.'-bullets'; ?> <?php 
-			echo $suffix; ?>"><?php
-			include (dirname(__FILE__).'/'.$this->getLayout().'_'.$this->theme.'.php');
-		?></div><?php
+		?><div id="mslider_<?php echo $this->widgetID;
+							?>" class="mslider mslider_main mslider_<?php echo $this->theme; ?> 
+							<?php
+							echo ($this->bullets) ? 'with-bullets' : ''; ?> 
+							<?php
+							echo ($this->progressbar) ? 'with-progressbar' : ''; ?> 
+							<?php
+							echo $this->bullets_style . '-bullets'; ?> 
+							<?php
+							echo $suffix; ?>">
+			<?php
+			include(dirname(__FILE__) . '/' . $this->getLayout() . '_' . $this->theme . '.php');
+			?></div>
+		<?php
 
 		// Progress bar
-		if ($this->progressbar)
-		{
-			?><div id="mslider_progressbar_<?php echo $this->widgetID; ?>" class="mslider-progressbar <?php
-			echo ($this->bullets) ? 'with-bullets' : ''; ?>"></div><?php
+		if ($this->progressbar) {
+		?><div id="mslider_progressbar_<?php echo $this->widgetID; ?>" class="mslider-progressbar 
+		<?php
+			echo ($this->bullets) ? 'with-bullets' : ''; ?>"></div>
+		<?php
 		}
-	
-	?></div><?php
-	
-?></div>
+		?></div>
+</div>
+<?php
+
+// Modal images
+if ($this->hoverBox && $this->hoverBoxZoomButton && $this->slider_images) {
+?><div class="mslider-modal">
+		<?php
+		echo HTMLHelper::_(
+			'bootstrap.renderModal',
+			'zoomSlider_' . $this->widgetID,
+			[
+				'title'       => '',
+				'closeButton' => true,
+				'height'      => '100%',
+				'width'       => 'auto',
+			],
+			'<img src="">'
+		);
+		?></div>
+<?php
+}
